5020ce5cacd629eaefc39af99000f4563f88b2aa
Serrial Error <serrialerror@outlook.com>
2024-10-28 18:30:49 -0700

    fixed color detection code

--- src/drive-functions.cpp
+++ src/drive-functions.cpp
@@ -15,22 +15,38 @@ IntakeControl::IntakeControl() :
 
 void IntakeControl::colorSorting() {
   hue = opticalsensor.hue();
-  if (hue < 30) {
-    Controller1.Screen.setCursor(1, 1);
-    Controller1.Screen.clearScreen();
-    Controller1.Screen.print("red");
-    ring = true;
-  }
-  if (hue > 170) {
-    Controller1.Screen.setCursor(1, 1);
-    Controller1.Screen.clearScreen();
-    Controller1.Screen.print("blue");
-    ring = false;
-  }
   if (intakeon) {
     // Color detection logic
+    if (hue < 30) {
+      Controller1.Screen.setCursor(1, 1);
+      Controller1.Screen.clearScreen();
+      Controller1.Screen.print("red");
+      ring = true;
+    }
+    if (hue > 170) {
+      Controller1.Screen.setCursor(1, 1);
+      Controller1.Screen.clearScreen();
+      Controller1.Screen.print("blue");
+      ring = false;
+    }
     if (opticalsensor.isNearObject()) {
-      if (!team) {
+      ringdetected = true;
+      intakevelocity = 80;
+    }
+    if (!team) {
+      if (distancesensor.objectDistance(inches) < 4) {
+        Controller1.Screen.setCursor(1, 1);
+        Controller1.Screen.clearScreen();
+        Controller1.Screen.print("object found");
+        wait(40, msec);
+        intakevelocity = 0;
+        intake.stop(brake);
+        wait(20, msec);
+        intake.spin(forward);
+        ring = false;
+        ringdetected = false;
+      }
+      else {
         if (distancesensor.objectDistance(inches) < 4) {
           Controller1.Screen.setCursor(1, 1);
           Controller1.Screen.clearScreen();
@@ -38,28 +54,12 @@ void IntakeControl::colorSorting() {
           wait(40, msec);
           intakevelocity = 0;
           intake.stop(brake);
-          wait(20, msec);
+    	  wait(20, msec);
           intake.spin(forward);
-          ring = false;
+          ring = true;
           ringdetected = false;
         }
-        else {
-          if (distancesensor.objectDistance(inches) < 4) {
-            Controller1.Screen.setCursor(1, 1);
-            Controller1.Screen.clearScreen();
-            Controller1.Screen.print("object found");
-            wait(40, msec);
-            intakevelocity = 0;
-            intake.stop(brake);
-            wait(20, msec);
-            intake.spin(forward);
-            ring = true;
-            ringdetected = false;
-          }
-        }
       }
-      ringdetected = true;
-      intakevelocity = 80;
     } 
     if (!ringdetected) {
       intakevelocity = 60;
